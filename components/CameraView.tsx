63663635fa2a605c6cddf7c2501e2a58d9e55ae0
David Carpenter <carpenterd777@gmail.com>
2021-09-04 22:26:15 -0400

    add empty views

---
+++ components/CameraView.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View } from 'react-native';
+
+const CameraView = () => {
+    return(
+        <View></View>
+    )
+}
+
+export default CameraView;
---
+++ components/TranscriptionView.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View } from 'react-native';
+
+const TranscriptionView = () => {
+    return(
+        <View></View>
+    )
+}
+
+export default TranscriptionView;
