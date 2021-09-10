aecd3d99b8b969ee89541efbc6fa199db41c3545
Carson McManus <carson.mcmanus1@gmail.com>
2021-09-10 09:45:35 -0400

    make the camera work

--- App.tsx
+++ App.tsx
@@ -1,11 +1,12 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+import CameraView from "./components/CameraView";
 
 export default function App(): JSX.Element {
 	return (
 		<View style={styles.container}>
-			<Text>Open up App.tsx to start working on your app!</Text>
+			<CameraView />
 			<StatusBar style="auto" />
 		</View>
 	);
