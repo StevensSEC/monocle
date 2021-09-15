d80415cec839f3323f7cca6def5a9188fd57acd0
Carson McManus <carson.mcmanus1@gmail.com>
2021-09-15 13:15:05 -0400

    temp: add react-native-mlkit-ocr and some debug code

--- components/CameraView.tsx
+++ components/CameraView.tsx
@@ -1,6 +1,9 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
 import React, { useEffect } from "react";
-import { StyleSheet, View } from "react-native";
+import { Button, StyleSheet, View } from "react-native";
 import { Camera } from "expo-camera";
+import MlkitOcr, { MlkitOcrResult } from "react-native-mlkit-ocr";
 
 const CameraView = (): JSX.Element => {
 	useEffect((): void => {
@@ -18,9 +21,36 @@ const CameraView = (): JSX.Element => {
 		})();
 	});
 
+	let camera: Camera;
+
+	async function takePicture(): Promise<void> {
+		if (camera) {
+			const picture = await camera.takePictureAsync();
+			// eslint-disable-next-line no-console
+			console.log(JSON.stringify(Object.keys(picture)));
+			// eslint-disable-next-line no-console
+			console.log(JSON.stringify(picture.uri));
+			// eslint-disable-next-line no-console
+			console.log(typeof MlkitOcr.detectFromUri);
+			// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+			const result: MlkitOcrResult = await MlkitOcr.detectFromUri(picture.uri);
+			// eslint-disable-next-line no-console
+			console.log(JSON.stringify(result));
+		}
+	}
+
 	return (
 		<View style={styles.container}>
-			<Camera style={{ flex: 1 }} type={Camera.Constants.Type.back} />
+			<Camera
+				style={{ flex: 1 }}
+				type={Camera.Constants.Type.back}
+				ref={r => {
+					if (r) {
+						camera = r;
+					}
+				}}
+			/>
+			<Button title="Take a picture" onPress={takePicture} />
 		</View>
 	);
 };
