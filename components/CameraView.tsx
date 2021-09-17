77546b9ebea4dacf3a743f13c201b5b50a404e27
David Carpenter <carpenterd777@gmail.com>
2021-09-17 17:30:28 -0400

    last image uri is saved to state

--- components/CameraView.tsx
+++ components/CameraView.tsx
@@ -2,17 +2,19 @@ import React, { useEffect, useState } from "react";
 import { StyleSheet, View } from "react-native";
 import { Camera, CameraCapturedPicture } from "expo-camera";
 
-import PhotoButton from "../components/PhotoButton/PhotoButton"
+import PhotoButton from "../components/PhotoButton/PhotoButton";
 
 const CameraView = (): JSX.Element => {
-	const [ref, setRef] = useState<Camera | null>()
+	const [ref, setRef] = useState<Camera | null>();
+	const [lastImageURI, setLastImageURI] = useState<string>();
 
-	const onPressPhotoButton = () : void => {
+	const onPressPhotoButton = (): void => {
 		void (async (): Promise<CameraCapturedPicture | undefined> => {
 			const picture = await ref?.takePictureAsync();
+			setLastImageURI(picture?.uri);
 			return picture;
 		})();
-	}
+	};
 
 	useEffect((): void => {
 		void (async (): Promise<void> => {
@@ -31,11 +33,14 @@ const CameraView = (): JSX.Element => {
 
 	return (
 		<View style={styles.container}>
-			<Camera 
-				ref={ref => {setRef(ref)}}
-				style={{ flex: 1 }} 
-				type={Camera.Constants.Type.back} />
-			
+			<Camera
+				ref={ref => {
+					setRef(ref);
+				}}
+				style={{ flex: 1 }}
+				type={Camera.Constants.Type.back}
+			/>
+			<PhotoButton onPress={onPressPhotoButton} color="#f4e1e6" />
 		</View>
 	);
 };
--- components/PhotoButton/PhotoButton.tsx
+++ components/PhotoButton/PhotoButton.tsx
@@ -1,18 +1,19 @@
-import React from 'react'
-import  { Button } from 'react-native'
+import React from "react";
+import { Button } from "react-native";
 interface PhotoButtonProps {
-    onPress: VoidFunction
-    color: string,
+	onPress: VoidFunction;
+	color: string;
 }
 
-const PhotoButton = (props: PhotoButtonProps) : JSX.Element => {
-    return (
-        <Button
-            onPress={props.onPress}
-            title=""
-            color={props.color}
-            accessibilityLabel="Button to take photo"/>
-    )
-}
+const PhotoButton = (props: PhotoButtonProps): JSX.Element => {
+	return (
+		<Button
+			onPress={props.onPress}
+			title=""
+			color={props.color}
+			accessibilityLabel="Button to take photo"
+		/>
+	);
+};
 
 export default PhotoButton;
