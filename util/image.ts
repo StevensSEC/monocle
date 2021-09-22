dc3b1948a471809994aab360a94a9335f286cc67
Carson McManus <carson.mcmanus1@gmail.com>
2021-09-22 16:27:09 -0400

    remove duplicate code

--- util/image.ts
+++ util/image.ts
@@ -1,5 +1,4 @@
 import * as tf from "@tensorflow/tfjs";
-import * as ImageManipulator from "expo-image-manipulator";
 import * as jpeg from "jpeg-js";
 
 export function imageToTensor(rawImageData: Uint8Array): tf.Tensor3D {
@@ -19,16 +18,5 @@ export function imageToTensor(rawImageData: Uint8Array): tf.Tensor3D {
 
 export function base64ImageToTensor(base64: string): tf.Tensor3D {
 	const rawImageData = tf.util.encodeString(base64, "base64");
-	const { width, height, data } = jpeg.decode(rawImageData);
-	// Drop the alpha channel info, we don't need it.
-	const buffer = new Uint8Array(width * height * 3);
-	let offset = 0; // offset into original data
-	for (let i = 0; i < buffer.length; i += 3) {
-		buffer[i] = data[offset];
-		buffer[i + 1] = data[offset + 1];
-		buffer[i + 2] = data[offset + 2];
-
-		offset += 4;
-	}
-	return tf.tensor3d(buffer, [height, width, 3]);
+	return imageToTensor(rawImageData);
 }
