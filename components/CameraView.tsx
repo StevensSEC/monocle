2134e08102800dc146f8b87d0c83bfb089f40576
Carson McManus <carson.mcmanus1@gmail.com>
2021-09-21 23:06:36 -0400

    bare minimum working object detection

--- components/CameraView.tsx
+++ components/CameraView.tsx
@@ -1,16 +1,37 @@
 import React, { useEffect, useState } from "react";
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, View, Text } from "react-native";
 import { Camera } from "expo-camera";
+import * as tf from "@tensorflow/tfjs";
+import "@tensorflow/tfjs-react-native";
+import * as cocoSsd from "@tensorflow-models/coco-ssd";
+import * as FileSystem from "expo-file-system";
 
 import PhotoButton from "../components/PhotoButton/PhotoButton";
+import { base64ImageToTensor } from "../util/image";
 
 const CameraView = (): JSX.Element => {
 	const [ref, setRef] = useState<Camera | null>();
 	const [lastImageURI, setLastImageURI] = useState<string>();
+	const [objectDetectionPreidictions, setObjectDetectionPreidictions] =
+		useState<cocoSsd.DetectedObject[]>();
+	const [status, setStatus] = useState<string>("Waiting for image");
 
 	const onPressPhotoButton = async (): Promise<void> => {
 		const picture = await ref?.takePictureAsync();
 		setLastImageURI(picture?.uri);
+
+		setStatus("Processing...");
+		const buffer = await FileSystem.readAsStringAsync(picture?.uri ?? "", {
+			encoding: FileSystem.EncodingType.Base64,
+		});
+		const imageTensor: tf.Tensor3D = base64ImageToTensor(buffer);
+
+		setStatus("Loading object detection model...");
+		const objectDetection = await cocoSsd.load();
+		setStatus("Detecting objects...");
+		const predictions = await objectDetection.detect(imageTensor);
+		setObjectDetectionPreidictions(predictions);
+		setStatus(`objects: ${JSON.stringify(objectDetectionPreidictions)}`);
 	};
 
 	useEffect((): void => {
@@ -25,6 +46,8 @@ const CameraView = (): JSX.Element => {
 					break;
 				}
 			}
+
+			await tf.ready();
 		})();
 	});
 
@@ -38,6 +61,8 @@ const CameraView = (): JSX.Element => {
 				type={Camera.Constants.Type.back}
 			/>
 			<PhotoButton onPress={onPressPhotoButton} color="#f4e1e6" />
+			<Text>{lastImageURI}</Text>
+			<Text>{status}</Text>
 		</View>
 	);
 };
