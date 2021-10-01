fb6879fbf6843217debcd78e1cca6463540a9f95
David Carpenter <carpenterd777@gmail.com>
2021-09-30 20:45:00 -0400

    Add app tests

---
+++ tests/App.test.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import TestRenderer from "react-test-renderer";
+import App from "../App";
+
+const tr = TestRenderer.create(<App />);
+
+it("renders something", () => {
+	expect(tr.toJSON()).toBeDefined();
+});
+
+it("renders correctly", () => {
+	expect(tr.toJSON()).toMatchSnapshot();
+});
