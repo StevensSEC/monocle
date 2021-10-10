7cc376812f84e4fed53ca27253a91edfdad1d1c5
Walker Bove <wvenusaur@gmail.com>
2021-10-10 11:43:41 -0400

    Added copy button. Also made a couple of style changes.

--- components/ResultContainer.tsx
+++ components/ResultContainer.tsx
@@ -1,5 +1,7 @@
+import { RotateWithOffset } from "@tensorflow/tfjs-core";
 import React, { useState } from "react";
-import { Pressable, SafeAreaView, StyleSheet, Text, TextInput } from "react-native";
+import { Pressable, SafeAreaView, StyleSheet, Text, TextInput, View } from "react-native";
+import * as Clipboard from "expo-clipboard";
 
 interface ResultContainerProps {
 	defaultText: string;
@@ -7,6 +9,13 @@ interface ResultContainerProps {
 
 const ResultContainer = ({ defaultText }: ResultContainerProps): JSX.Element => {
 	const [result, changeResult] = useState<string>(defaultText);
+	const [copyButtonStyle, changeCopyButtonStyle] = useState<Object>(styles.copyButton);
+	const [resetButtonStyle, changeResetButtonStyle] = useState<Object>(styles.resetButton);
+
+	const copyResultToClipboard = (result: string): void => {
+		Clipboard.setString(result);
+	};
+
 	return (
 		<SafeAreaView>
 			<TextInput
@@ -17,17 +26,43 @@ const ResultContainer = ({ defaultText }: ResultContainerProps): JSX.Element => 
 				multiline={true}
 				numberOfLines={5}
 				autoCapitalize="none"
+				accessibilityRole="text"
 				accessibilityLabel={"Transcription Result is... " + result}
 			/>
-			<Pressable
-				onPress={() => {
-					changeResult(defaultText);
-				}}
-				style={styles.resetButton}
-				accessibilityLabel={"Reset text in box back to detected results"}
-			>
-				<Text style={styles.resetButtonText}>Reset Text</Text>
-			</Pressable>
+			<View style={styles.horizontalContainer}>
+				<Pressable
+					onPressIn={() => {
+						changeCopyButtonStyle(styles.copyButtonPressed);
+					}}
+					onPressOut={() => {
+						changeCopyButtonStyle(styles.copyButton);
+					}}
+					onPress={() => {
+						copyResultToClipboard(result);
+					}}
+					style={[styles.button, copyButtonStyle]}
+					accessibilityRole="button"
+					accessibilityLabel={"Copy text in box to your device's clipboard"}
+				>
+					<Text style={styles.buttonText}>Copy Text To Clipboard</Text>
+				</Pressable>
+				<Pressable
+					onPressIn={() => {
+						changeResetButtonStyle(styles.resetButtonPressed);
+					}}
+					onPressOut={() => {
+						changeResetButtonStyle(styles.resetButton);
+					}}
+					onPress={() => {
+						changeResult(defaultText);
+					}}
+					style={[styles.button, resetButtonStyle]}
+					accessibilityRole="button"
+					accessibilityLabel={"Reset text in box back to detected results"}
+				>
+					<Text style={styles.buttonText}>Reset Text</Text>
+				</Pressable>
+			</View>
 		</SafeAreaView>
 	);
 };
@@ -38,22 +73,50 @@ const styles = StyleSheet.create({
 		borderWidth: 2,
 		padding: 2,
 		margin: 2,
-		marginTop: 20,
+		marginTop: 250,
 		textAlign: "center",
 		textAlignVertical: "center",
+	},
+
+	horizontalContainer: {
+		flexDirection: "row",
+		justifyContent: "space-between",
+	},
+
+	button: {
+		borderWidth: 2,
+		borderRadius: 20,
+		paddingVertical: 20,
+		paddingHorizontal: 10,
+		marginVertical: 20,
+		marginHorizontal: 5,
+	},
+
+	copyButton: {
+		backgroundColor: "#777711",
+		borderColor: "#000000",
+		flex: 60,
+	},
+
+	copyButtonPressed: {
+		backgroundColor: "#666600",
+		borderColor: "#000000",
+		flex: 60,
 	},
 
 	resetButton: {
 		backgroundColor: "#bb2222",
 		borderColor: "#000000",
-		borderWidth: 2,
-		borderRadius: 20,
-		padding: 20,
-		marginVertical: 20,
-		marginHorizontal: 120,
+		flex: 40,
 	},
 
-	resetButtonText: {
+	resetButtonPressed: {
+		backgroundColor: "#aa1111",
+		borderColor: "#000000",
+		flex: 40,
+	},
+
+	buttonText: {
 		color: "#ffffff",
 		textAlign: "center",
 		textAlignVertical: "center",
