627542e613416fed97428a822e13bc00e8f7f803
Carson McManus <carson.mcmanus1@gmail.com>
2021-10-15 11:07:16 -0400

    add optic middleware for monitoring http api changes

--- api-server/main.ts
+++ api-server/main.ts
@@ -1,8 +1,14 @@
 import express, { Express } from "express";
+import { OpticMiddleware } from "@useoptic/express-middleware";
 import { apirouter } from "./api";
 
 // eslint-disable-next-line @typescript-eslint/no-unnecessary-type-assertion
 const app = express() as Express;
+app.use(
+	OpticMiddleware({
+		enabled: process.env.NODE_ENV !== "production",
+	})
+);
 app.use("/api", apirouter);
 
 const port = process.env.PORT ?? 3000;
