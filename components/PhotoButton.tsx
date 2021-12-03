aa5f368892800336e83a3ef393f6343e07583df9
Joshua Gorman <90810000+endevii@users.noreply.github.com>
2021-12-02 21:19:32 -0500

    Styled button

--- components/PhotoButton.tsx
+++ components/PhotoButton.tsx
@@ -1,25 +1,58 @@
 import React from "react";
-import { Text, StyleSheet, Pressable } from "react-native";
+import { Text, StyleSheet, Pressable, Image, View } from "react-native";
 interface PhotoButtonProps {
 	onPress: VoidFunction;
 }
 
 const PhotoButton = (props: PhotoButtonProps): JSX.Element => {
 	return (
-		<Pressable onPress={props.onPress} style={styles.button} accessibilityLabel="Take photo">
-			<Text>Take Photo</Text>
-		</Pressable>
+		<View style={styles.container}>
+			<Pressable
+				onPress={props.onPress}
+				style={styles.button}
+				accessibilityLabel="Take photo"
+			>
+				<Image source={require("../assets/Monocle-Logo-Gray.png")} style={styles.img} />
+			</Pressable>
+			{/* <Text>Take Photo</Text> */}
+		</View>
 	);
 };
 
 const styles = StyleSheet.create({
-	button: {
-		borderColor: "#000000",
-		borderWidth: 2,
-		borderRadius: 10,
-		margin: 20,
+	container: {
+		width: "100%",
+		height: "25%",
+		alignItems: "center",
+		justifyContent: "center",
+		position: "absolute",
+		backgroundColor: "transparent",
+		borderColor: "transparent",
 		padding: 20,
+		bottom: 0,
+	},
+	button: {
+		alignItems: "center",
+		justifyContent: "center",
+		borderColor: "black",
+		backgroundColor: "rgba(77, 84, 89, 0.5)",
+		opacity: 0.75,
+		borderWidth: 3,
+		borderRadius: 100,
+		padding: 15,
 		textAlign: "center",
+		shadowColor: "#303838",
+		shadowOffset: { width: 0, height: 5 },
+		shadowRadius: 10,
+		shadowOpacity: 0.5,
+	},
+	img: {
+		height: 70,
+		width: 70,
+		backgroundColor: "transparent",
+		padding: 2,
+		resizeMode: "contain",
+		opacity: 0.75,
 	},
 });
 
